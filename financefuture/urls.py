9c303e41b70af400cc3ea6bc81463ac3114edd68
aayushkananu <aayushka.budhathoki@bison.howard.edu>
2024-12-02 19:39:54 -0500

    landing page

--- financefuture/urls.py
+++ financefuture/urls.py
@@ -20,4 +20,5 @@ from .views import CreateUserView, LoginView
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('accounts/', include('allauth.urls')),
 ]
